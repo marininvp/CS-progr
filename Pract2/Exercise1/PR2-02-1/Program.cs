1a0e947ec91f9e923c407f8eb75a40f44c3b086d
student <student@C320U02>
2020-09-11 11:58:08 +0300

    pr1 exercise1

--- Pract2/Exercise1/PR2-02-1/Program.cs
+++ Pract2/Exercise1/PR2-02-1/Program.cs
@@ -6,10 +6,24 @@ namespace PR2_02_1
   {
     static void Main(string[] args)
     {
-      string s = "Microsoft .NET Framework 2 Application Development Foundation";
-      string[] sa = s.Split(" ");
-      Array.Sort(sa);
-      s = string.Join(" ", sa); Console.WriteLine(s);
+      SByte a = 0; 
+      Byte b = 0; 
+      Int16 c = 0; 
+      Int32 d = 0;
+      Int64 e = 0; 
+      string s= "";
+      Exception ex = new Exception();
+
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object о in types)
+      {
+        string type;
+        if (о.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", о.GetType(), type);
+      }
     }
   }
 }
