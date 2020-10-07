5da372f1eec88604acb5ac689be932999ffaa206
marininvp <marininvp@yandex.ru>
2020-10-07 11:45:34 +0300

    pr4.02-02

---
+++ Pract4/Practicum2/Exercise2/PR-04-02-02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace PR_04_02_02
+{
+  class Program
+  {
+   
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+
+      queue.Enqueue("First");
+      queue.Enqueue("Second");
+      queue.Enqueue("Third");
+      queue.Enqueue("Fourth");
+      Stack stack = new Stack();
+      stack.Push("First");
+      stack.Push("Second");
+      stack.Push("Third");
+      stack.Push("Fourth");
+
+  
+      while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+      }
+
+    }
+  }
+
+}
+
