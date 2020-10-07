714f5898beab79df62acbc8c10ae22f1458f3898
marininvp <marininvp@yandex.ru>
2020-10-07 11:40:41 +0300

    pr4.02-01

---
+++ Pract4/Practicum2/PR-04-02-01/PR-04-02-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace PR_04_02_01
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+
+      queue.Enqueue("First");
+      queue.Enqueue("Second");
+      queue.Enqueue("Third"); 
+      queue.Enqueue("Fourth");
+    while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+    }
+  }
+
+}
