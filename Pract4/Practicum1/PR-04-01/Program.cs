1dc0fc84960e0a1a85abea5c60944188258f0a49
marininvp <marininvp@yandex.ru>
2020-10-07 11:33:53 +0300

    pr4.1

---
+++ Pract4/Practicum1/PR-04-01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace PR_04_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList();
+      myList.Add("Первый");
+      myList.Add("Второй");
+      myList.Add("Третий");
+      myList.Add("Четвертый");
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      // Сортировка при помощи стандартного объекта сравнения 
+      myList.Sort();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+    }
+
+  }
+}
