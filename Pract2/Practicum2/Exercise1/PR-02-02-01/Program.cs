b2e990ebe4d072ae0ee7e7b510ebc19c3df7bec3
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 16:23:11 +0300

    pr02.02-01

---
+++ Pract2/Practicum2/Exercise1/PR-02-02-01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace PR_02_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText(@"D:\hg.txt"); 
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?"); 
+            writer.Close();
+        }
+    }
+}
