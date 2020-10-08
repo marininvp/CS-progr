5cc29418a938daf3586fc80452361349db39f625
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 16:28:52 +0300

    pr02.02-02

---
+++ Pract2/Practicum2/Exercise2/PR-02-02-02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace PR_02_02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = File.OpenText(@"D:\hh.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
