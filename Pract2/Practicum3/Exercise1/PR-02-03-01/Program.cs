509ac2e0428c80e9dcfd9452bc85495cf9a20c80
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 18:30:02 +0300

    pr.02.03-01

---
+++ Pract2/Practicum3/Exercise1/PR-02-03-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace PR_02_03_01
+{
+    class Program
+    {
+        static void CompressFile(string inFilename,string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            
+            int theByte =sourceFile.ReadByte();
+            while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte);
+                theByte = sourceFile.ReadByte();
+            }
+            compStream.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            CompressFile(@"D:\hh.txt", @"D:\hh.txt.gz");
+        }
+    }
+}
