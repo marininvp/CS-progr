172deee0058bfe935cf7121c2b9706fd15dc3cd0
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 16:03:28 +0300

    pr02.01-01

---
+++ Pract2/Practicum1/Exercise2/PR-02-01-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace PR_02_01_02
+{
+    class Program
+    {
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher =new FileSystemWatcher(Environment.SystemDirectory);
+
+            // C#
+            watcher.EnableRaisingEvents = true;
+            watcher.Filter = "*.ini*";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            Console.ReadKey();
+            
+        }
+    }
+}
