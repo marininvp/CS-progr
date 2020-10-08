38d9b528b5096c6107e6c8a3f395b201b3926c3f
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 19:18:54 +0300

    pr02.04-02

---
+++ Pract2/Practicum4/Exercise2/PR-02-04-02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace PR_02_04_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("99 Bottles of Beer on the wall,99 Bottles of Beer");
+            userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Файл отсутствует");
+                Environment.Exit(0);
+            }
+
+            userStream = new IsolatedStorageFileStream("UserSettings.set",FileMode.Open, userStore); 
+            StreamReader userReader = new StreamReader(userStream);
+            string contents = userReader.ReadToEnd();
+
+            Console.WriteLine(userReader);
+        }
+    }
+}
