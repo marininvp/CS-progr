eeaaa6cb3f47fb7c00288ca0fb219df206f7e268
marininvp <kkkwww.ll@yandex.ru>
2020-10-08 15:22:08 +0300

    pr2.01

---
+++ Pract2/Practicum1/Exercise1/PR-02-01-01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace PR_02_01_01
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            // Показать все файлы
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("File: {0}", file.FullName);
+            }
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                ShowDirectory(subDir);
+            }
+
+
+        }
+        
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory);
+            ShowDirectory(dir);
+        }
+
+    }
+}
